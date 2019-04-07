ee71049abc19c8502431920ff51cbe692eb0f7e5
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-07 19:15:04 +1000

    Sepearated routes, added route for updating single model from a given resource

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,8 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
-    Route::get('resources', 'ResourceController@index');
-    Route::get('resources/{resource}', 'ResourceController@show');
+    Route::get('resources', 'ResourcesController@index');
+
+    Route::get('resources/{resource}', 'ResourceController@index');
+    Route::get('resources/{resource}/{id}', 'ResourceController@show');
+    Route::put('resources/{resource}/{id}', 'ResourceController@update');
 });
 
 Route::get('/{view?}', 'AppController@show')->where('view', '(.*)');
