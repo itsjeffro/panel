e41adab313eff72832d699ba4b3caa043d03587f
Jeffery Nielsen <jefftoddnielsen@gmail.com>
2019-04-09 18:56:57 +1000

    - Updated stub to include id - Removed form builder

--- src/Fields/Field.php
+++ src/Fields/Field.php
@@ -5,14 +5,51 @@ namespace Itsjeffro\Panel\Fields;
 abstract class Field
 {
     /**
+     * @var string
+     */
+    public $name = '';
+    
+    /**
+     * @var bool
+     */
+    public $index = false;
+    
+    /**
      * @param string $name
      * @param string $column
-     * @return FieldBuilder
      */
-    public static function make(string $name = '', string $column = '')
+    public function __construct(string $name = '', string $column = '')
     {
-        $name = empty($name) ? get_called_class() : $name;
+        $this->name = $name;
+        $this->column = $column;
+    }
 
-        return (new FieldBuilder($name, $column));
+    /**
+     * Instantiate a new instance of the child class utilising
+     * methods from the extended Field class to use.
+     *
+     * @param string $name
+     * @param string $column
+     * @return \Field
+     */
+    public static function make(string $name = '', string $column = ''): Field
+    {
+        $childClass = static::class;
+        $name = empty($name) ? $childClass : $name;
+        $column = empty($column) ? strtolower($name) : $column;
+
+        return new new $childClass($name, $column);
+    }
+    
+    /**
+     * Set the field to be indexed in the table.
+     *
+     * @return self
+     */
+    public function index(): self
+    {
+        $this->index = true;
+
+        return $this;
     }
 }
