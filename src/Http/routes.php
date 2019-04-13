f36e21dced1aec37bcf2c56634065027af80b456
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-13 22:42:06 +1000

    Added post route for resource create

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -10,6 +10,7 @@ Route::prefix('api')->group(function () {
     Route::get('resources/{resource}', 'ResourceController@index');
     Route::get('resources/{resource}/{id}', 'ResourceController@show');
     Route::put('resources/{resource}/{id}', 'ResourceController@update');
+    Route::post('resources/{resource}', 'ResourceController@store');
 });
 
 Route::get('/{view?}', 'AppController@show')->where('view', '(.*)');
