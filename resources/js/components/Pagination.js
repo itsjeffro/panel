a9e21297295f6bc7a675b4f2e531dac146f6824d
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-14 14:11:09 +1000

    Added handlePageClick prop

--- resources/js/components/Pagination.js
+++ resources/js/components/Pagination.js
@@ -4,12 +4,14 @@ const Pagination = (props) => {
   const {
     total,
     per_page,
-    current_page
+    current_page,
+    handlePageClick
   } = props;
 
   const total_pages = Math.ceil(total / per_page);
 
   let pages = [];
+
   for (let page = 1; page <= total_pages; page++) {
     pages.push(page);
   }
@@ -20,19 +22,21 @@ const Pagination = (props) => {
         Total: {total}
       </div>
       <ul className="pagination">
-        <li className="page-item">
-          <a className="page-link" href="#" aria-label="Previous">
+        <li className={'page-item' + (current_page <= 1 ? ' disabled' : '')}>
+          <a className="page-link" aria-label="Previous" href="#" onClick={e => handlePageClick(e, 1)}>
             <span aria-hidden="true">&laquo;</span>
             <span className="sr-only">Previous</span>
           </a>
         </li>
 
         {pages.map(page =>
-          <li className="page-item"><a className="page-link" href="#">{page}</a></li>
+          <li className={'page-item' + (page === current_page ? ' active' : '')} key={'page-' + page}>
+            <a className="page-link" href="#" onClick={e => handlePageClick(e, page)}>{page}</a>
+          </li>
         )}
 
-        <li className="page-item">
-          <a className="page-link" href="#" aria-label="Next">
+        <li className={'page-item' + (current_page >= total_pages ? ' disabled' : '')}>
+          <a className="page-link" aria-label="Next" href="#" onClick={e => handlePageClick(e, total_pages)}>
             <span aria-hidden="true">&raquo;</span>
             <span className="sr-only">Next</span>
           </a>
