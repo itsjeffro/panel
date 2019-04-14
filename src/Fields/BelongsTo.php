b9dac7a284d9694e3bd3168d2f16dbff1ecdbc43
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-14 22:35:05 +1000

    Added BelongsTo field

---
+++ src/Fields/BelongsTo.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Itsjeffro\Panel\Fields;
+
+class BelongsTo extends Field
+{
+    /**
+     * @var bool
+     */
+    public $showOnCreate = true;
+
+    /**
+     * @var bool
+     */
+    public $showOnUpdate = true;
+
+    /**
+     * @var bool
+     */
+    public $showOnDetail = true;
+
+    /**
+     * @var bool
+     */
+    public $showOnIndex = true;
+
+    /**
+     * @var string
+     */
+    public $component = 'BelongsTo';
+}
