f6aa8cb62e1442ed00c1da5559caa04325c90682
Jeff Nielsen <jeff.nielsen@4mation.com.au>
2019-04-20 19:56:30 +1000

    Renamed page to ResourceIndexPage

--- resources/js/Root.js
+++ resources/js/Root.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import DashboardPage from './pages/DashboardPage';
-import ResourcePage from './pages/ResourcePage';
+import ResourceIndexPage from './pages/ResourceIndexPage';
 import ResourceCreatePage from './pages/ResourceCreatePage';
 import ResourceEditPage from './pages/ResourceEditPage';
 import ResourceViewPage from './pages/ResourceViewPage';
@@ -14,7 +14,7 @@ class Root extends React.Component {
         <div>
           <Switch>
             <Route exact path="/" component={DashboardPage} />
-            <Route exact path="/resources/:resource" component={ResourcePage} />
+            <Route exact path="/resources/:resource" component={ResourceIndexPage} />
             <Route exact path="/resources/:resource/create" component={ResourceCreatePage} />
             <Route exact path="/resources/:resource/:id" component={ResourceViewPage} />
             <Route exact path="/resources/:resource/:id/edit" component={ResourceEditPage} />
