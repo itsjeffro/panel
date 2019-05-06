dc902fa10bf55e140d68778136257ada601891d9
Jeffery Nielsen <jefftoddnielsen@gmail.com>
2019-05-07 07:34:41 +1000

    Added phpunit xml, travis yml, and example test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Deploy\Tests\Unit\ProviderRepository;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
