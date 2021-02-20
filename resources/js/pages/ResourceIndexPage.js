aaa06fc78f48cd2defd925862f4a4d10cf397853
jeff.nielsen <jeff.n@covergenius.com>
2021-02-21 10:32:25 +1100

    Moved table to own component

--- resources/js/pages/ResourceIndexPage.js
+++ resources/js/pages/ResourceIndexPage.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
-import Pagination from "../components/Pagination";
-import IndexComponent from "../fields/IndexComponent";
+import ResourceTable from "../components/ResourceTable";
 
 class ResourceIndexPage extends React.Component {
   constructor(props) {
@@ -15,8 +14,8 @@ class ResourceIndexPage extends React.Component {
       search: '',
     };
 
-    this.onPageClick = this.onPageClick.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+    this.onPageClick = this.onPageClick.bind(this);
     this.onDeleteClick = this.onDeleteClick.bind(this);
     this.onDropdownBulkClick = this.onDropdownBulkClick.bind(this);
   }
@@ -66,8 +65,7 @@ class ResourceIndexPage extends React.Component {
   }
 
   /**
-   * @param event
-   * @param page
+   * Load paged results based on page click.
    */
   onPageClick(event, page) {
     event.preventDefault();
@@ -95,10 +93,6 @@ class ResourceIndexPage extends React.Component {
 
   /**
    * Handle delete and reload resources.
-   *
-   * @param event
-   * @param resource
-   * @param id
    */
   onDeleteClick(event, resource, id) {
     axios
@@ -158,73 +152,14 @@ class ResourceIndexPage extends React.Component {
           </div>
         </div>
 
-        <div className="table-card card">
-          <div className="card-header">
-            <div className="form-check form-check-inline">
-              <input className="form-check-input" type="checkbox" />
-
-              <div className="dropdown">
-                <button className="btn pb-0 dropdown-toggle" onClick={this.onDropdownBulkClick}></button>
-                <div className={'dropdown-menu' + (isDropdownBulkShown ? ' show' : '')}>
-                  <a className="dropdown-item" href="#">Bulk Delete</a>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <table className="table mb-0">
-            <thead>
-              <tr>
-                <th width="1%" className="border-top-0 text-right">
-                  {' '}
-                </th>
-                {fields.map(field =>
-                  <th className="border-top-0" key={field.column}>{field.name}</th>
-                )}
-                <th className="border-top-0 text-right">
-                  {' '}
-                </th>
-              </tr>
-            </thead>
-
-            <tbody>
-              {(resource.model_data.data).map(model =>
-                <tr key={model.id}>
-                  <td width="1%">
-                    <div className="form-check form-check-inline">
-                      <input className="form-check-input" type="checkbox" />
-                    </div>
-                  </td>
-                  {fields.map(field =>
-                    <td key={model.id + '-' + field.column}>
-                      <IndexComponent
-                        component={field.component}
-                        model={model}
-                        field={field}
-                      />
-                    </td>
-                  )}
-                  <td className="text-right">
-                    <Link className="btn btn-link" to={'/resources/' + params.resource + '/' + model.id}><span className="typcn typcn-eye-outline" /></Link>{' '}
-                    <Link className="btn btn-link" to={'/resources/' + params.resource + '/' + model.id + '/edit'}><span className="typcn typcn-edit" /></Link>{' '}
-                    <button
-                      className="btn btn-link"
-                      onClick={e => this.onDeleteClick(e, params.resource, model.id)}
-                    ><span className="typcn typcn-trash" /></button>
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-          <div className="card-footer card-pagination">
-            <Pagination
-              total={resource.model_data.total}
-              per_page={resource.model_data.per_page}
-              current_page={resource.model_data.current_page}
-              handlePageClick={this.onPageClick}
-            />
-          </div>
-        </div>
+        <ResourceTable
+          onPageClick={ this.onPageClick }
+          onDropdownBulkClick={ this.onDropdownBulkClick }
+          onDeleteClick={ this.onDeleteClick }
+          isDropdownBulkShown={ isDropdownBulkShown }
+          resource={ resource }
+          params={ params }
+        />
       </div>
     )
   }
