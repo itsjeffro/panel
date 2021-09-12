e40e4e7e80d5218150cc99d33bd0797f5191cbf0
jeff.nielsen <jeff.n@covergenius.com>
2021-09-12 13:02:48 +1000

    Moved detault field to own component

--- resources/js/fields/FieldComponent.js
+++ resources/js/fields/FieldComponent.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PasswordField from './Password/PasswordField';
 import BelongsToField from "./BelongsTo/BelongsToField";
 import TextareaField from "./Textarea/TextareaField";
+import DefaultComponent from "./DefaultComponent";
 
 const FieldComponent = (props) => {
   const components = {
@@ -17,28 +18,10 @@ const FieldComponent = (props) => {
     value
   } = props;
 
-  const ComponentName = components[field.component];
-
+  const ComponentName = components[field.component] || DefaultComponent;
   const column = field.isRelationshipField ? field.foreignKey : field.column;
   const hasError = errors !== null && errors.hasOwnProperty(column);
   const messageError = errors !== null && errors.hasOwnProperty(column) ? errors[column][0] : '';
-
-  if (typeof ComponentName == 'undefined') {
-    return (
-      <span>
-        <input
-          className={'form-control' + (hasError ? ' is-invalid' : '')}
-          type="text"
-          name={column}
-          value={value}
-          onChange={handleInputChange}
-          placeholder={field.name}
-        />
-
-        {hasError ? <div className="invalid-feedback">{messageError}</div> : ''}
-      </span>
-    )
-  }
 
   return (
     <ComponentName
