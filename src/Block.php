670ce52c7f88847314cb2324b4c42319c0222630
jeff.nielsen <jeff.n@covergenius.com>
2021-09-24 16:53:01 +1000

    Added block to group fields

---
+++ src/Block.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Itsjeffro\Panel;
+
+use Itsjeffro\Panel\Fields\Field;
+
+class Block
+{
+    /**
+     * @var string
+     */
+    private $blockName;
+
+    /**
+     * @var Field[]
+     */
+    private $fields;
+
+    /**
+     * @param Field[] $fields
+     */
+    public function __construct(string $blockName, array $fields)
+    {
+        $this->blockName = $blockName;
+        $this->fields = $fields;
+    }
+
+    public function getName(): string
+    {
+        return $this->blockName;
+    }
+
+    public function getFields(): array
+    {
+        return $this->fields;
+    }
+}
