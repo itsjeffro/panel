8d135773557fa15c17cda1044d7512208cce68d9
jeff.nielsen <jeff.n@covergenius.com>
2021-09-24 16:55:36 +1000

    Added methods to return model name and plural name

--- src/Resource.php
+++ src/Resource.php
@@ -2,10 +2,27 @@
 
 namespace Itsjeffro\Panel;
 
-abstract class Resource
+use Illuminate\Support\Str;
+use Itsjeffro\Panel\Contracts\ResourceInterface;
+
+abstract class Resource implements ResourceInterface
 {
     /**
-     * Return resource's fields.
+     * Return resource's defined fields.
      */
     abstract public function fields(): array;
+
+    public function modelName(): string
+    {
+        $model = explode('\\', $this->model);
+
+        return end($model);
+    }
+
+    public function modelPluralName(): string
+    {
+        $model = explode('\\', $this->model);
+
+        return Str::plural(end($model));
+    }
 }
