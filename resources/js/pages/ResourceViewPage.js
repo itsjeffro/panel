d27401ed8b439a9145c93889d09b3fc54cc210e7
jeff.nielsen <jeff.n@covergenius.com>
2021-09-25 12:17:00 +1000

    Loop through groups

--- resources/js/pages/ResourceViewPage.js
+++ resources/js/pages/ResourceViewPage.js
@@ -5,14 +5,9 @@ import DetailComponent from "../fields/DetailComponent";
 import ResourceTable from "../components/ResourceTable";
 
 class ResourceViewPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      resource: null,
-      relationships: {}
-    };
-  }
+  state = {
+    resource: null
+  };
 
   componentWillMount() {
     const {params} = this.props.match;
@@ -33,43 +28,8 @@ class ResourceViewPage extends React.Component {
     axios
       .get('/panel/api/resources/' + resourceUri + '/' + id)
       .then(response => {
-        const relationships = response.data.relationships;
-
-        this.loadRelationships(relationships);
-
         this.setState({resource: response.data});
       });
-  }
-
-  /**
-   * Load any relationships that this resource might have.
-   *
-   * @param {array} relationships
-   * @returns void
-   */
-  loadRelationships(relationships) {
-    Object.keys(relationships).map((relationship) => {
-      const models = relationships[relationship];
-
-      Object.keys(models).map((model) => {
-        axios
-          .get('/panel/api/resources/' + models[model].table)
-          .then((response) => {
-            this.setState((prevState) => {
-              return {
-                ...prevState.relationships,
-                relationships: {
-                  [relationship]: {
-                    [model]: response.data
-                  }
-                }
-              }
-            })
-          }, (error) => {
-            console.log(error);
-          });
-      })
-    })
   }
 
   render() {
@@ -86,61 +46,58 @@ class ResourceViewPage extends React.Component {
       )
     }
 
-    const fields = resource.fields.filter(field => {
-      return field.showOnDetail;
-    });
-
     return (
       <div className="content">
         <div className="container">
-          <div className="row">
-            <div className="col-md-6">
-              <div className="page-heading">
-                <h2>{resource.name.singular} Details</h2>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className="form-group text-md-right">
-                <Link
-                  className="btn btn-primary btn-sm"
-                  to={'/resources/' + params.resource + '/' + params.id + '/edit'}
-                >Edit</Link>
-              </div>
-            </div>
-          </div>
+          { Object.keys(resource.groups).map((groupKey, index) => {
+            const group = resource.groups[groupKey];
 
-          <div className="card">
-            <div className="list-group list-group-flush">
-              { fields.map(field =>
-                <div className="list-group-item" key={field.column}>
-                  <div className="row">
-                    <div className="col-xs-12 col-md-2">
-                      <strong>{field.name}</strong>
-                    </div>
-                    <div className="col-xs-12 col-md-10">
-                      <DetailComponent
-                        component={field.component}
-                        model={resource.model_data}
-                        field={field}
-                      />
-                    </div>
-                  </div>
-                </div>
-              ) }
-            </div>
-          </div>
-
-          { (relationships.hasMany ? Object.keys(relationships.hasMany) : []).map((model) => {
-            const resource = relationships.hasMany[model];
+            if (group.fields.length === 0) {
+              return <></>
+            }
 
             return (
-              <div key={resource.name.plural} className="mt-5">
-                <ResourceTable
-                  resourceUri={ model }
-                />
+              <div key={ groupKey }>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="page-heading">
+                      <h2>{ group.name }</h2>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    { index === 0 ? <div className="form-group text-md-right">
+                      <Link
+                        className="btn btn-primary btn-sm"
+                        to={'/resources/' + params.resource + '/' + params.id + '/edit'}
+                      >Edit</Link>
+                    </div> : '' }
+                  </div>
+                </div>
+
+                <div className="card mb-4">
+                  <div className="list-group list-group-flush">
+                    { group.fields.map((field) =>
+                      <div className="list-group-item" key={field.column}>
+                        <div className="row">
+                          <div className="col-xs-12 col-md-2">
+                            <strong>{field.name}</strong>
+                          </div>
+                          <div className="col-xs-12 col-md-10">
+                            <DetailComponent
+                              component={field.component}
+                              model={resource.model_data}
+                              field={field}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    ) }
+                  </div>
+                </div>
               </div>
             )
-          }) }
+          })}
+
         </div>
       </div>
     )
