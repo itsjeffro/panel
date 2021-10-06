208c9250871cefb3f249c812de27adc2c0c81e0d
jeff.nielsen <jeff.n@covergenius.com>
2021-10-07 08:03:27 +1100

    Removed action list

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     // Actions
-    Route::get('resources/{resource}/actions', 'ResourceActionController@index');
     Route::post('resources/{resource}/actions/{action}', 'ResourceActionController@handle');
 
     // Fields
