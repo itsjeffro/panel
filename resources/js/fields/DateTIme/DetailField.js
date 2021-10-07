d2343537877a0f5bd353d28f4ab7e7753ea5396f
jeff.nielsen <jeff.n@covergenius.com>
2021-10-07 12:20:38 +1100

    Fixed hour display, id 0 then display as 12

--- resources/js/fields/DateTIme/DetailField.js
+++ resources/js/fields/DateTIme/DetailField.js
@@ -23,6 +23,8 @@ const DetailField = (props) => {
   let day = date.getDate();
   let year = date.getFullYear();
   let hours = date.getHours() % 12;
+  hours = hours === 0 ? 12 : hours;
+
   let minutes = date.getMinutes();
   let meridiem = date.getHours() >= 12 ? 'PM' : 'AM';
   let timezoneName = Intl.DateTimeFormat().resolvedOptions().timeZone;
