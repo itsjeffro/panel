872931b51c8de2b234798ae4ec5485cd00e326cd
jeff.nielsen <jeff.n@covergenius.com>
2021-10-08 19:55:24 +1100

    Added route names

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     // Actions
-    Route::post('resources/{resource}/actions/{action}', 'ResourceActionController@handle');
+    Route::post('resources/{resource}/actions/{action}', 'ResourceActionController@handle')->name('panel.resources.actions.handle');
 
     // Fields
-    Route::get('resources/{resource}/fields', 'ResourceFieldController@show');
+    Route::get('resources/{resource}/fields', 'ResourceFieldController@show')->name('panel.resources.fields.show');
 
     // Resources
     Route::get('resources/{resource}', 'ResourceController@index')->name('panel.resources.index');
-    Route::get('resources/{resource}/{id}', 'ResourceController@show');
-    Route::get('resources/{resource}/{id}/edit', 'ResourceController@edit');
-    Route::put('resources/{resource}/{id}', 'ResourceController@update');
-    Route::post('resources/{resource}', 'ResourceController@store');
-    Route::delete('resources/{resource}/{id}', 'ResourceController@destroy');
+    Route::get('resources/{resource}/{id}', 'ResourceController@show')->name('panel.resources.show');
+    Route::get('resources/{resource}/{id}/edit', 'ResourceController@edit')->name('panel.resources.edit');
+    Route::put('resources/{resource}/{id}', 'ResourceController@update')->name('panel.resources.update');
+    Route::post('resources/{resource}', 'ResourceController@store')->name('panel.resources.store');
+    Route::delete('resources/{resource}/{id}', 'ResourceController@destroy')->name('panel.resources.destroy');
 });
 
 Route::get('/{view?}', 'AppController@show')->where('view', '(.*)');
