77a25a9fdaf708571c623d273067dc541c3530c1
jeff.nielsen <jeff.n@covergenius.com>
2021-10-10 10:34:54 +1100

    Removed redundant methods for now

--- src/Actions/Action.php
+++ src/Actions/Action.php
@@ -4,19 +4,5 @@ namespace Itsjeffro\Panel\Actions;
 
 abstract class Action
 {
-    /**
-     * Action constructor.
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Create instance of Action.
-     */
-    public static function make(): self
-    {
-        return new static();
-    }
+    //
 }
