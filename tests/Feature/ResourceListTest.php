9b0a6e93a83b047510ef7552bcfad7fd2272fedf
jeff.nielsen <jeff.n@covergenius.com>
2021-10-09 13:37:57 +1100

    Added search test case

--- tests/Feature/ResourceListTest.php
+++ tests/Feature/ResourceListTest.php
@@ -2,6 +2,7 @@
 
 namespace Itsjeffro\Panel\Tests\Feature;
 
+use Itsjeffro\Panel\Tests\Models\User;
 use Itsjeffro\Panel\Tests\TestCase;
 
 class ResourceListTest extends TestCase
@@ -18,6 +19,8 @@ class ResourceListTest extends TestCase
 
     public function test_user_can_see_listed_resources()
     {
+        $user = factory(User::class)->create();
+
         $response = $this->json(
             'GET',
             route('panel.resources.index', ['resource' => 'users'])
@@ -26,7 +29,50 @@ class ResourceListTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJson([
-                'data' => [],
+                'data' => [
+                    [
+                        'resourceId' => $user->getKey(),
+                        'resourceName' => $user->name,
+                        'resourceFields' => [
+                            [
+                                'component' => 'Text',
+                                'field' => [
+                                    'attribute' => 'id',
+                                    'name' => 'ID',
+                                    'value' => $user->getKey(),
+                                ],
+                                'resource' => 'users',
+                                'resourceId' => $user->getKey(),
+                                'resourceName' => $user->getKey(),
+                                'relationship' => null,
+                            ],
+                            [
+                                'component' => 'Text',
+                                'field' => [
+                                    'attribute' => 'name',
+                                    'name' => 'Name',
+                                    'value' => $user->name,
+                                ],
+                                'resource' => 'users',
+                                'resourceId' => $user->getKey(),
+                                'resourceName' => $user->name,
+                                'relationship' => null,
+                            ],
+                            [
+                                'component' => 'Text',
+                                'field' => [
+                                    'attribute' => 'email',
+                                    'name' => 'Email',
+                                    'value' => $user->email,
+                                ],
+                                'resource' => 'users',
+                                'resourceId' => $user->getKey(),
+                                'resourceName' => $user->email,
+                                'relationship' => null,
+                            ]
+                        ],
+                    ],
+                ],
                 'meta' => [
                     'actions' => [
                         ['name' => 'Bulk Delete', 'slug' => 'bulk-delete'],
@@ -44,5 +90,23 @@ class ResourceListTest extends TestCase
                     ],
                 ],
             ]);
+    }
+
+    public function test_user_can_search_listed_resources()
+    {
+        $user1 = factory(User::class)->create();
+        $user2 = factory(User::class)->create();
+
+        $response = $this->json(
+            'GET',
+            route('panel.resources.index', ['resource' => 'users', 'search' => $user1->getKey()])
+        );
+
+        $jsonData = $response
+            ->assertStatus(200)
+            ->json('data');
+
+        $this->assertCount(1, $jsonData);
+        $this->assertSame($user1->name, $jsonData[0]['resourceName']);
     }
 }
