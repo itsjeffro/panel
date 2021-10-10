6b8455817d0194df4a268c3152867ec1a2a05033
jeff.nielsen <jeff.n@covergenius.com>
2021-10-10 13:29:51 +1100

    wip

--- resources/js/pages/DashboardPage.js
+++ resources/js/pages/DashboardPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Drawer from "../components/Drawer";
+import {IconBrandGithub} from "@tabler/icons";
 
 class DashboardPage extends React.Component {
   constructor(props) {
@@ -9,8 +10,22 @@ class DashboardPage extends React.Component {
   render() {
     return(
       <div className="content">
-        <div className="page-heading">
-          <h1>Getting Started</h1>
+        <div className="container">
+          <div className="page-heading">
+            <h2>Getting Started</h2>
+
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Documentation</h5>
+                <p>Additional documentation can be found in the <code>/docs</code> directory.</p>
+
+                <a
+                  href="https://github.com/itsjeffro/panel"
+                  title="Github"
+                ><IconBrandGithub style={{ verticalAlign: 'text-bottom' }} height={18} width={18}/> itsjeffro/panel</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
